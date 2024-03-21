81dda6de76487bd88d58b19af013e41ae2790341
Rudra044 <rudrarajput560@gmail.com>
2024-03-21 18:17:06 +0530

    update

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Profile
-
+from flask_bcrypt import Bcrypt 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
+bcrypt = Bcrypt(app) 
 
 @app.route('/register', methods=['POST'])
 def add_user():
@@ -19,13 +19,13 @@ def add_user():
     if email_id and username and password:
         if Profile.query.filter_by(email_id=email_id).first() or Profile.query.filter_by(username=username).first():
             return jsonify({'error': 'User with this email or username already exists'})
-        new_user = Profile(email_id=email_id, username=username, password=password)
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = Profile(email_id=email_id, username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return jsonify({'message': 'User created successfully'})
     else:
         return jsonify({'error': 'Email, username, and password are required'})
-
 
 @app.route('/login', methods=['POST'])
 def create_user():
@@ -33,14 +33,13 @@ def create_user():
     username = data.get('username')
     password = data.get('password')
     if username and password:
-        user = Profile.query.filter_by(username=username, password=password).first()
-        if user:
+        user = Profile.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
             return jsonify({'message': 'Login successful'})
         else:
             return jsonify({'error': 'Invalid username or password'})
     else:
         return jsonify({'error': 'Username and password are required'})
-
 
 if __name__ == "__main__":
     with app.app_context():
